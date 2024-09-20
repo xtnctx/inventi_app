208f0c4fb30c97cf95c022676d3580ceb5b8c48a
Ryan Christopher Bahillo <ryanbahillo.10@gmail.com>
2024-09-21 01:46:10 +0800

    connect to deployed server

--- lib/api/env.dart
+++ lib/api/env.dart
@@ -2,13 +2,13 @@
 
 class Env {
   // Physical device
-  static String BASE_URL = "http://192.168.1.13:8000";
+  // static String BASE_URL = "http://192.168.1.13:8000";
 
   // Emulator
   // static String BASE_URL = "http://10.0.2.2:8000";
 
   // Production
-  // static String BASE_URL = "http://spyxanya.pythonanywhere.com/";
+  static String BASE_URL = "https://xtnctx.pythonanywhere.com/";
 
   static String TEXT_GENERATOR_URL =
       "https://o7q6ka26qs232rmbtpbrxghy6u0vyrup.lambda-url.ap-southeast-1.on.aws/";
